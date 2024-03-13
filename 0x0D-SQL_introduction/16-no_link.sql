542539882366b3ae6ca4291c583bdf0982298837
Cephas-Muhia <Cephaschronixx@gmail.com>
2024-03-13 00:45:08 -0700

    Say my name

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -4,4 +4,4 @@
 -- Records should be listed by descending score
 -- The database name will be passed as an argument to the mysql command
 
-SELECT * score as a value, name FROM second_table ORDER BY value  DESC;
+SELECT score, name FROM second_table WHERE name = NOT NULL AND  name != ""  ORDER BY score  DESC;
