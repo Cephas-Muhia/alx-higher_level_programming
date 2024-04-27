12bbbdbf292b3a32ea328ed34a97d675a0559ed5
Cephas-Muhia <Cephaschronixx@gmail.com>
2024-04-27 18:38:20 +0300

    Error code Error code

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -2,19 +2,16 @@
 """Sends a request to a given URL and displays the response body.
 Usage: ./7-error_code.py <URL>
   - Handles HTTP errors.
-  """
-
+"""
 import sys
 import requests
 
+
 if __name__ == "__main__":
-    url = syt.argv[1]
+    url = sys.argv[1]
 
     r = requests.get(url)
     if r.status_code >= 400:
-        print("error code: {}".format(r.status_code))
+        print("Error code: {}".format(r.status_code))
     else:
-        print( r.text)
-
-    
-                                                                     
+        print(r.text)
