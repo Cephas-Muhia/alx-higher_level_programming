515784e0f832019c90879d8484eb1e9de8dee9bf
Cephas-Muhia <Cephaschronixx@gmail.com>
2024-04-27 18:28:34 +0300

    Error code Error code

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Script that takes in url,sends a url request and displays the body
+with the condition
+HTTP status code >= 400 brings an error
+and the resulting value error is printed """
+
+import syt
+import request
+
+if __name__ == "__main__":
+    url = syt.argv[1]
+
+    r = requests.get(url)
+    if r.status_code >= 400:
+        print("error code: {}".format(r.status_code))
+    else:
+        print( r.text)
+
+    
+                                                                     
